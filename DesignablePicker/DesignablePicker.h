906f4e5af073ec5c49fa8e31dd957cb77e4158ee
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2017-09-27 12:00:54 +0200

    Initial Commit

---
+++ DesignablePicker/DesignablePicker.h
@@ -0,0 +1,19 @@
+//
+//  DesignablePicker.h
+//  DesignablePicker
+//
+//  Created by Oleksandr Pronin on 27.09.17.
+//  Copyright © 2017 Alexander Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DesignablePicker.
+FOUNDATION_EXPORT double DesignablePickerVersionNumber;
+
+//! Project version string for DesignablePicker.
+FOUNDATION_EXPORT const unsigned char DesignablePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DesignablePicker/PublicHeader.h>
+
+
